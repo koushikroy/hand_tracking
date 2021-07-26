4759526245c9d73441b0fe6177e6d5fb893038f7
koushikroy <rkoushikroy2@gmail.com>
2021-07-27 05:26:33 +0600

    uploading necessary files

---
+++ test.py
@@ -0,0 +1,32 @@
+# Using Keyboard module in Python
+import keyboard
+
+
+
+# It writes the content to output
+keyboard.write("GEEKS FOR GEEKS\n")
+
+keyboard.press_and_release('left, space')
+
+
+# it blocks until ctrl is pressed
+keyboard.wait('Ctrl')
+
+GEEKS FOR GEEKS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
